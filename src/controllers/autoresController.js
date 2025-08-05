5a4c20575b8b711e46101b80b0e7487e3ecf3a51
Moisés Brito <moisesbritojp@hotmail.com>
2025-08-05 15:11:14 -0300

    aplicando validações do mongoose

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -1,6 +1,6 @@
 //import mongoose from "mongoose";
 import NaoEncontrado from "../erros/NaoEncontrado.js";
-import autores from "../models/Autor.js";
+import {autores} from "../models/index.js";
 
 class AutorController {
 
